44a127cb78542b502a83632da66bd4ddcf70a241
humEyra1064 <humeyraercanfurkanyilmaz@gmail.com>
2023-02-12 21:57:58 +0300

    card scss added

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -4,8 +4,13 @@ const Card = (props) => {
   const{title,desc,image} = props
   return (
     <div className="cards">
-      <h1 className="title">{title}</h1>
+
+      <div className="title">
+        <h1 >{title}</h1>
+      </div>
+      
       <img src={image} alt="" />
+      
       <div className="card-over">
         <p>{desc}</p>
       </div>
